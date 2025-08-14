a17879226c854bd691fe197c8b82f4c69cfea2a9
Narayan Gautam <ng4111894@gmail.com>
2025-08-15 00:34:55 +0545

    Add a custom badge widget

---
+++ lib/apps/productivity/presentation/widgets/common/custom_badge.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:varosa_tech/apps/productivity/data/models/badge_model.dart';
+import '../../../../../themes/app_theme.dart';
+
+class CustomBadge extends StatelessWidget {
+  const CustomBadge(this.badge, {super.key});
+  final BadgeModel badge;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+      decoration: BoxDecoration(
+        color: badge.color.withAlpha((0.1 * 255).toInt()),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Text(
+        badge.label,
+        style: AppTextStyles.bodySmall.copyWith(
+          color: badge.color,
+          fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+}
